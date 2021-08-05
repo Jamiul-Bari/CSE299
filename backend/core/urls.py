45e336fd6847c3f8ac46f85c05b43285bdc6ae8c
Warsi-Omrao-Khan-Shuvo <warsi.shuvo@northsouth.edu>
2021-08-06 00:43:39 +0600

    Customized JWT to add username and email in the response from API

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
 
 from . import views
 
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 
     path('', views.get_routes, name='routes'),
     path('grocery-items/', views.get_grocery_items, name='grocery-items'),
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .models import GroceryItem
 from .grocery_items import grocery_items
 from .serializers import GroceryItemSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 # Create your views here.
