9fa9c278af24d79a50dd4eac88293e25bef9d0da
Jamiul Bari <jamiul.bari98@gmail.com>
2021-08-05 19:22:49 +0600

    Add to cart function

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap';
 
-function CartPage() {
+import Message from '../components/Message';
+import {addToCart} from '../actions/CartActions';
+
+function CartPage({match, location, history}) {
+    const grocery_item_id = match.params.id;
+    const qty = location.search
+        ? Number(location.search.split('=')[1])
+        : 1
+
+    const dispatch = useDispatch();
+
+    const cart = useSelector(state => state.cart)
+    const {grocery_in_cart} = cart;
+
+    useEffect(() => {
+        if (grocery_item_id) {
+            dispatch(addToCart(grocery_item_id, qty))
+        }
+    }, [dispatch, grocery_item_id, qty]);
+
+
     return (
         <div>
             Cart
