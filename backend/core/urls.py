671149209352f4d3b7404a110d8a4cddd83eae73
Warsi-Omrao-Khan-Shuvo <warsi.shuvo@northsouth.edu>
2021-08-06 00:27:03 +0600

    Installed and configured JSON Web Tokens package simplejwt

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 from . import views
 
 urlpatterns = [
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
     path('', views.get_routes, name='routes'),
     path('grocery-items/', views.get_grocery_items, name='grocery-items'),
     path('grocery-item/<str:pk>', views.get_grocery_item, name='grocery-item'),
