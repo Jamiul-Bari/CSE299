a11f9dc70ee5bd2a0a400e6fd9eb1810f6485350
Agomon01 <jakaria.jabed@northsouth.edu>
2021-08-07 20:10:57 +0600

    Backend url and view logic added for user profile update

--- backend/core/urls/users_urls.py
+++ backend/core/urls/users_urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
 
     path('profile/', views.get_user_profile, name='user-profile'),
+    path('profile/update', views.update_user_profile, name='update-user-profile'),
     path('', views.get_users, name='users'),
 ]
