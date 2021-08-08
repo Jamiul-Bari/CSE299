5e004744f9abfb62a916a38155d3751ba9bcb880
Jamiul Bari <jamiul.bari98@gmail.com>
2021-08-08 16:24:00 +0600

    Place Order page created. Changed few fields of Order model in backend

--- backend/core/models.py
+++ backend/core/models.py
@@ -37,8 +37,8 @@ class Review(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True)
-    taxPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    vat = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    deliveryCharge = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     totalPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     isPaid = models.BooleanField(default=False)
     paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
 import CheckoutPage from './pages/CheckoutPage';
 import PaymentPage from './pages/PaymentPage';
+import PlaceOrderScreen from './pages/PlaceOrderScreen';
 
 function App() {
     return (
@@ -25,6 +26,7 @@ function App() {
                     <Route path='/register' component={RegisterPage}/>
                     <Route path='/profile' component={ProfilePage}/>
                     <Route path='/checkout' component={CheckoutPage}/>
+                    <Route path='/place-order' component={PlaceOrderScreen}/>
                     <Route path='/payment' component={PaymentPage}/>
                     <Route path='/grocery-item/:id' component={GroceryItemPage}/>
                     <Route path='/cart/:id?' component={CartPage}/>
