66d929c1e7f4d8a5e67faa43ba95261a78c1f697
Warsi-Omrao-Khan-Shuvo <warsi.shuvo@northsouth.edu>
2021-08-18 12:03:06 +0600

    Created page for editing specific grocery item. Registered it in App.js

---
+++ frontend/src/pages/GroceryItemEditPage.js
@@ -0,0 +1,168 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux'
+
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import FormContainer from '../components/FormContainer';
+
+import { listGroceryItemDetails } from '../actions/GroceryItemActions';
+
+
+function GroceryItemEditPage({ match, history }) {
+
+    const grocery_item_id = match.params.id;
+
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState(0);
+    const [image, setImage] = useState('');
+    const [brand, setBrand] = useState('');
+    const [category, setCategory] = useState('');
+    const [countInStock, setCountInStock] = useState(0);
+    const [description, setDescription] = useState('');
+
+    const dispatch = useDispatch();
+
+    const groceryItemDetails = useSelector(state => state.groceryItemDetails);
+    const { error, loading, grocery_item } = groceryItemDetails;
+
+
+    useEffect(() => {
+
+        if (!grocery_item.name || grocery_item._id !== Number(grocery_item_id)) {
+            dispatch(listGroceryItemDetails(grocery_item_id));
+        }
+        else {
+            setName(grocery_item.name);
+            setPrice(grocery_item.price);
+            setImage(grocery_item.image);
+            setBrand(grocery_item.brand);
+            setCategory(grocery_item.category);
+            setCountInStock(grocery_item.countInStock);
+            setDescription(grocery_item.description);
+        }
+
+    }, [dispatch, grocery_item, grocery_item_id, history])
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        // update
+    }
+
+    return (
+        <div>
+
+            <Link to='/admin/grocery-item'>
+                Go Back
+            </Link>
+
+            <FormContainer>
+                <h1>Edit Grocery Item</h1>
+
+                {
+                    loading ? <Loader />
+                        : error
+                            ? <Message variant='danger'>{error}</Message>
+                            : (
+                                <Form onSubmit={submitHandler}>
+
+                                    <Form.Group controlId='name'>
+                                        <Form.Label>Name</Form.Label>
+                                        <Form.Control
+                                            type='text'
+                                            placeholder='Enter Name'
+                                            value={name}
+                                            onChange={(e) => setName(e.target.value)}
+                                        >
+
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group controlId='price'>
+                                        <Form.Label>Price</Form.Label>
+                                        <Form.Control
+                                            type='number'
+                                            placeholder='Enter Price'
+                                            value={price}
+                                            onChange={(e) => setPrice(e.target.value)}
+                                        >
+
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group controlId='image'>
+                                        <Form.Label>Image</Form.Label>
+                                        <Form.Control
+                                            type='text'
+                                            placeholder='Enter Image'
+                                            value={image}
+                                            onChange={(e) => setImage(e.target.value)}
+                                        >
+
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group controlId='brand'>
+                                        <Form.Label>Brand</Form.Label>
+                                        <Form.Control
+                                            type='text'
+                                            placeholder='Enter Brand'
+                                            value={brand}
+                                            onChange={(e) => setBrand(e.target.value)}
+                                        >
+
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group controlId='countInStock'>
+                                        <Form.Label>Stock</Form.Label>
+                                        <Form.Control
+                                            type='number'
+                                            placeholder='Enter stock'
+                                            value={countInStock}
+                                            onChange={(e) => setCountInStock(e.target.value)}
+                                        >
+
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group controlId='category'>
+                                        <Form.Label>Category</Form.Label>
+                                        <Form.Control
+                                            type='text'
+                                            placeholder='Enter Category'
+                                            value={category}
+                                            onChange={(e) => setCategory(e.target.value)}
+                                        >
+
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group controlId='description'>
+                                        <Form.Label>Description</Form.Label>
+                                        <Form.Control
+                                            type='text'
+                                            placeholder='Enter Description'
+                                            value={description}
+                                            onChange={(e) => setDescription(e.target.value)}
+                                        >
+
+                                        </Form.Control>
+                                    </Form.Group>
+
+
+                                    <Button type='submit' variant='primary'>
+                                        Update
+                                    </Button>
+
+                                </Form>
+                            )
+                }
+
+            </FormContainer>
+        </div>
+    )
+}
+
+export default GroceryItemEditPage;
