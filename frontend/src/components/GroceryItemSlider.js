349a2b53c66eda19a2af1bd969db79fcd163e78b
Jamiul Bari <jamiul.bari98@gmail.com>
2021-08-24 21:55:14 +0600

    Fixed frontend import issues

--- frontend/src/components/GroceryItemSlider.js
+++ frontend/src/components/GroceryItemSlider.js
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Carousel, Image } from 'react-bootstrap';
 
-import { Loader } from './Loader';
-import { Message } from './Message';
+import Loader from './Loader';
+import Message from './Message';
 
 import { listHitGroceryItems } from '../actions/GroceryItemActions';
 
